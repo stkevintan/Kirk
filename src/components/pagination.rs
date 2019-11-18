a74d00dade0534c242ce4d4f32bd419021a89937
stkevintan <stkevintan@zju.edu.cn>
2019-11-18 09:45:57 +0800

    feat: add pagination component

--- src/components/pagination.rs
+++ src/components/pagination.rs
@@ -1,4 +1,5 @@
 use crate::common::types;
+use log::*;
 use yew::prelude::*;
 use yew::Callback;
 #[derive(PartialEq, Properties)]
@@ -14,6 +15,8 @@ pub struct Pagination {
 
 pub enum Msg {
   TurnPage(u32),
+  TurnLeft,
+  TurnRight,
 }
 
 impl Component for Pagination {
@@ -34,23 +37,58 @@ impl Component for Pagination {
         if self.props.pagination.current == target {
           return false;
         }
-        self.props.on_page_change.emit(target)
+        self.props.on_page_change.emit(target);
+        true
+      }
+      Msg::TurnLeft => {
+        trace!("turn left, current is {}", self.props.pagination.current);
+        if self.props.pagination.current > 1 {
+          self.props.pagination.current -= 1;
+          return true;
+        }
+        false
+      }
+      Msg::TurnRight => {
+        if self.props.pagination.current < self.props.pagination.last {
+          self.props.pagination.current += 1;
+          return true;
+        }
+        false
       }
     }
-    true
   }
 
   fn view(&self) -> Html<Self> {
+    let is_first = self.props.pagination.current == 1;
+    let is_last = self.props.pagination.current == self.props.pagination.last;
     html! {
       <div class="pagination-wrap">
         <ul class="pagination">
+        <li
+          onclick=|_|Msg::TurnLeft
+          class=format!("pagination-item pagination-chevron pagination-chevron-left {}", if is_first {"disabled"} else {""})>
+            <i class="iconfont icon-chevron-left"/>
+          </li>
         {
           for self.get_index().map(move |current| {
-            html! {
-              <li onclick=|_|Msg::TurnPage(current) class="pagination-item">{current}</li>
+            if current == -1 {
+                return html! {
+                  <li class="pagination-item pagination-ellipsis pagination-ellipsis-left"><i  class="iconfont icon-dots-horizontal" /></li>
+                }
+            } else if current == 0  {
+               return html! {
+                <li class="pagination-item pagination-ellipsis pagination-ellipsis-right"><i  class="iconfont icon-dots-horizontal" /></li>
+              }
+            }else {
+               return  html! {
+                <li onclick=|_|Msg::TurnPage(current as u32) class={format!("pagination-item pagination-{}", self.item_class(current as u32))}>{current}</li>
+              }
             }
           })
         }
+        <li onclick=|_|Msg::TurnRight class=format!("pagination-item pagination-chevron pagination-chevron-right {}", if is_last {"disabled"} else {""})>
+            <i class="iconfont icon-chevron-right"/>
+        </li>
         </ul>
       </div>
     }
@@ -58,14 +96,14 @@ impl Component for Pagination {
 }
 
 impl Pagination {
-  fn get_index(&self) -> Box<dyn Iterator<Item = u32>> {
-    let current = self.props.pagination.current;
-    let last = self.props.pagination.last;
+  fn get_index(&self) -> Box<dyn Iterator<Item = i32>> {
+    let current = self.props.pagination.current as i32;
+    let last = self.props.pagination.last as i32;
     // if pages is lower than 10, ouput it directly
     if last < 10 {
       return Box::new(1..=last);
     }
-    let head = vec![1, 0];
+    let head = vec![1, -1];
     let tail = vec![0, last];
     if current < 4 {
       return Box::new((1..5).chain(tail.clone()));
